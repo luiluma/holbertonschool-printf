3d753204c5bccd91c478953e79e240ba25bbedcd
luiluma <luma24lopez@gmail.com>
2022-11-17 13:27:49 -0500

    borrador

--- Header.h
+++ Header.h
@@ -3,9 +3,9 @@
 //Libraries:
 #include<stdlib.h>
 #include<stdio.h>
+#include<stdarg.h>
 
-
-
-
+//Prototypes//
+int _printf(const char *format, ...);
 
 #endif HEADER_H
--- pruebasLuma.c
+++ pruebasLuma.c
@@ -1,1 +1,13 @@
 #include "Header.h"
+
+int (*get_func(const char *s, int a))(va_list)
+{
+    f_funct arr[] = {
+
+        {"c", print_chr}, 
+        {"s", print_str},
+		{"i", print_int}, 
+        {"d", print_int},
+        {"%"}, print_for}, 
+        {NULL, NULL},}
+}
