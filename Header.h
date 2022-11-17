84bb7ba65322517bf411dd2b3c2bc22763afaee5
jegomezV <jegomezvalencia@gmail.com>
2022-11-16 19:24:00 -0500

    avances|16/11/2022|

--- Header.h
+++ Header.h
@@ -1,10 +1,18 @@
 #ifdef HEADER_H
 #define HEADER_H
+
 //Libraries:
 #include<stdlib.h>
 #include<stdio.h>
+#include <stdarg.h>
 
 
+//Estructura de la funcion:
+typedef struct f_funct
+{
+	char *f_funct;
+	int (*f)(int a, int b);
+} f_funct;
 
 
 
