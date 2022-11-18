4d0ff69651dec698e7b98bf41dcd568e951a8b42
jegomezV <jegomezvalencia@gmail.com>
2022-11-18 12:15:24 -0500

    ajustes

--- _printf.c
+++ _printf.c
@@ -1,25 +1,24 @@
 #include "main.h"
-
-
 /*
-* _printf - function printf
-*
-* @format: string format
-*
-* Retorno: print value chars
-*/
-int _printf(const char *format, ...){
-    unsigned int i = 0, count = 0;
-
-if (!format)
-    exit (1);
-while (format[i])
+ * _printf - function printf
+ * @format: string format
+ * Retorno: print value chars
+ */
+int _printf(const char *format, ...)
 {
-    if (format [i] != '%')
-        {
-            _putchar (format[i]);
-            count ++;
-        }
-}
+    unsigned int i = 0, count = 0;
+    va_list list;
 
+    va_start(list, format);
+
+    if (!format)
+        exit(1);
+    while (format[i])
+    {
+        if (format[i] != '%')
+        {
+            _putchar(format[i]);
+            count++;
+        }
+    }
 }
--- main.h
+++ main.h
@@ -1,21 +1,11 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-
-//Libraries//
-#include<stdlib.h>
-#include<stdio.h>
 #include <stdarg.h>
-#include<string.h>
 #include <unistd.h>
 
-
-//Estructura de la funcion:
-typedef struct f_funct
-{
-	char *f_funct;
-	int (*f)(int a, int b);
-} f_funct;
-
-
+int _printf(const char *format, ...); /*Prototype of _printf*/
+int print_char(char c);
+int print_string(char *s);
+int get_spec_format(char *);
 
 #endif
