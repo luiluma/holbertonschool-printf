a411b448706cf0f5525864ed301f3cf5b0bb0a42
luiluma <luma24lopez@gmail.com>
2022-11-18 09:01:01 -0500

    prueba

--- _printf.c
+++ _printf.c
@@ -1,3 +1,45 @@
 #include "Header.h"
+#include <stdarg.h>
 
-int _printf(const char *format, ...);
+/*
+* get_func - selecciona chars para la conversion
+*
+* @s: verificacion
+*
+* Retorno: puntero a funcion
+*/
+int (* get_func(const char *s,))(va_list)
+{
+    f_funct arr[] = {
+
+        {"c", print_chr}, 
+        {"s", print_str},
+		{"i", print_int}, 
+        {"d", print_int},
+        {"%"}, print_for},
+};
+    int i;
+    i = 0;
+
+    while (i < 5)
+    {
+        if (s == arr[i].s[0])
+			return (arr[i].f);
+
+		i++;
+	}
+	return (NULL);
+
+
+/*
+* _printf - function printf
+*
+* @format: string format
+*
+* Retorno: print value chars
+*/
+
+int _printf(const char *format, ...)
+{
+    
+}
